3b0029798fa960f294a6c91e0cfe7d1a1abcef00
Codeprofs5 <164242985+Codeprofs5@users.noreply.github.com>
2024-03-30 01:37:47 +0530

    Add new file

---
+++ py8.py
@@ -0,0 +1,14 @@
+# Python program to demonstrate 
+# logical and operator 
+
+a = 10
+b = 10
+c = -10
+
+if a > 0 and b > 0: 
+	print("The numbers are greater than 0") 
+
+if a > 0 and b > 0 and c > 0: 
+	print("The numbers are greater than 0") 
+else: 
+	print("Atleast one number is not greater than 0") 
