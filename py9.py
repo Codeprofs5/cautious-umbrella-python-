d2d67383e8d6fb9f6516c6e0e354b5fa439510e7
Codeprofs5 <164242985+Codeprofs5@users.noreply.github.com>
2024-04-04 15:36:22 +0530

    Add new file

---
+++ py9.py
@@ -0,0 +1,16 @@
+# Python program to demonstrate 
+# logical or operator 
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0: 
+	print("Either of the number is greater than 0") 
+else: 
+	print("No number is greater than 0") 
+
+if b > 0 or c > 0: 
+	print("Either of the number is greater than 0") 
+else: 
+	print("No number is greater than 0") 
